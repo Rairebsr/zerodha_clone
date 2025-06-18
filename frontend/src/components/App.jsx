e8308633c6c3ea6325c64fde0ca7ba88878cb4a8
Rairebsr <150224883+Rairebsr@users.noreply.github.com>
2025-06-18 22:03:12 +0530

    Add files via upload

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Products from './pages/Products'
+import Pricing from './pages/Pricing'
+import Supports from './pages/Supports'
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <NavBar/>
+      
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/products' element={<Products/>}/>
+        <Route path='/pricing' element={<Pricing/>}/>
+        <Route path='/support' element={<Supports/>}/>
+      </Routes>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
