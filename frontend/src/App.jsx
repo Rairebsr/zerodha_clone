065e4b9229af94b738d24916fa38fb2a4b6004e8
Hemashree Mudalaje <137807977+hemashree2004@users.noreply.github.com>
2025-06-18 22:15:00 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,19 +6,21 @@ import Products from './pages/Products'
 import Pricing from './pages/Pricing'
 import Supports from './pages/Supports'
 import NavBar from './components/NavBar'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
-      <NavBar/>
+      <NavBar />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/products' element={<Products/>}/>
-        <Route path='/pricing' element={<Pricing/>}/>
-        <Route path='/support' element={<Supports/>}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='/pricing' element={<Pricing />} />
+        <Route path='/support' element={<Supports />} />
       </Routes>
       
+      <Footer />
     </div>
   )
 }
